057aefab8fb2e14aebe55f4c30bde1c957c427c5
Archie <archie_l_carpenter@hotmail.com>
2023-05-28 15:55:33 +0100

    feat: colours

---
+++ lib/colours.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+class Colours {
+
+  /// Base colours
+  final Color EdenBase = const Color(0xFF13505B);
+  final Color MalchaliteBase = const Color(0xFF0EC86A);
+  final Color LondonHueBase = const Color(0xFFC8B2D1);
+
+  /// Tints
+  final Color EdenLight = const Color(0xFF218A9D);
+  final Color EdenDark = const Color(0xFF0B3037);
+
+  final Color MalachiteLight = const Color(0xFF22F087);
+  final Color MalachiteDark = const Color(0xFF087840);
+
+  final Color LondonHueLight = const Color(0xFFDED1E3);
+  final Color LondonHueDark = const Color(0xFF805791);
+}
